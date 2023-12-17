96bdc5b82a70475b4a2af2650d274ce868b0ad19
Andrew Walwema <awalwema@gmail.com>
2023-12-16 17:45:07 -0800

    add mantine and basic layout

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import React, { Suspense } from "react";
 import { BlitzLayout } from "@blitzjs/next";
+import { Horizontal, Vertical } from "mantine-layout-components";
+import { AppShell, Footer, Header, Navbar, Text } from "@mantine/core";
 
 type Props = {
   title?: string;
@@ -8,6 +10,7 @@ type Props = {
   maxWidth?: number;
 };
 const Layout: BlitzLayout<Props> = ({ title, maxWidth = 800, children }) => {
+  const thisYear = new Date().getFullYear();
   return (
     <>
       <Head>
@@ -15,16 +18,40 @@ const Layout: BlitzLayout<Props> = ({ title, maxWidth = 800, children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Suspense fallback="Loading...">
-        <div
-          style={{
-            width: "100%",
-            maxWidth: maxWidth,
-          }}
-        >
-          {children}
-        </div>
-      </Suspense>
+      <AppShell
+        padding="md"
+        // navbar={
+        //   <Navbar width={{ base: 300 }} height={500} p="xs">
+        //     {/* Navbar content */}
+        //   </Navbar>
+        // }
+        header={
+          <Header height={45} p="xs">
+            <Horizontal debug fullH>
+              <Text fw="bold">Eventio</Text>
+            </Horizontal>
+          </Header>
+        }
+        footer={
+          <Footer height={30}>
+            <Horizontal fullH fullW center>
+              <Text fz="xs" color="dimmed">
+                copyright {thisYear}{" "}
+              </Text>
+            </Horizontal>
+          </Footer>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Vertical fullW fullH>
+          <Suspense fallback="Loading...">{children}</Suspense>
+        </Vertical>
+      </AppShell>
     </>
   );
 };
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,13 +2,22 @@ import { ErrorBoundary, AppProps } from "@blitzjs/next";
 import React, { Suspense } from "react";
 import { withBlitz } from "src/blitz-client";
 import { RootErrorFallback } from "@/core/components/RootErrorFallback";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
-      <Suspense fallback="Loading...">
-        <Component {...pageProps} />
-      </Suspense>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <Suspense fallback="Loading...">
+          <Component {...pageProps} />
+        </Suspense>
+      </MantineProvider>
     </ErrorBoundary>
   );
 }
